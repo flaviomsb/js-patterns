2818a445d30da89ec5be92285036488766d7f6c0
Flavio Barboza <flavio.barboza@alchemysystems.com>
2022-07-23 18:28:40 -0500

    reverse integer exercise

---
+++ src/data-structures/4/reverse-integer/reverseInteger.js
@@ -0,0 +1,10 @@
+import reverseString from '../reverse-string/reverseString';
+
+/**
+ * Reverses an integer
+ * @param {number} num
+ * @returns {number}
+ */
+export default function reverseInteger(num) {
+  return parseInt(reverseString(num.toString()), 10) * Math.sign(num);
+}
---
+++ src/data-structures/4/reverse-integer/reverseInteger.test.js
@@ -0,0 +1,14 @@
+import reverseInteger from './reverseInteger';
+
+describe('Integer reversal', () => {
+  test.each([
+    [1000, 1],
+    [1005, 5001],
+    [2, 2],
+    [789, 987],
+    [125689, 986521],
+    [-23, -32],
+  ])('Reverse of %s is %s', (num, expectedResult) => {
+    expect(reverseInteger(num)).toEqual(expectedResult);
+  });
+});
