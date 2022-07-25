3e18de99df280311b048df9a1e4ecb1d9301940a
Flavio Barboza <flavio.barboza@alchemysystems.com>
2022-07-24 21:30:13 -0500

    array chunk exercise

---
+++ src/data-structures/4/array-chunk/arrayChunk.js
@@ -0,0 +1,18 @@
+/**
+ * Given an array and chunk size, divide the array into many subarrays
+ * where each subarray is of length size
+ * @param {Array} arr
+ * @param {number} size
+ * @returns {Array}
+ */
+export default function arrayChunk(arr, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
---
+++ src/data-structures/4/array-chunk/arrayChunk.test.js
@@ -0,0 +1,35 @@
+import arrayChunk from './arrayChunk';
+
+describe('Array chunks', () => {
+  test('function arrayChunk exists', () => {
+    expect(typeof arrayChunk).toEqual('function');
+  });
+
+  test('chunk divides an array of 10 elements with chunk size 2', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const chunked = arrayChunk(arr, 2);
+
+    expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
+  });
+
+  test('chunk divides an array of 3 elements with chunk size 1', () => {
+    const arr = [1, 2, 3];
+    const chunked = arrayChunk(arr, 1);
+
+    expect(chunked).toEqual([[1], [2], [3]]);
+  });
+
+  test('chunk divides an array of 5 elements with chunk size 3', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const chunked = arrayChunk(arr, 3);
+
+    expect(chunked).toEqual([[1, 2, 3], [4, 5]]);
+  });
+
+  test('chunk divides an array of 13 elements with chunk size 5', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const chunked = arrayChunk(arr, 5);
+
+    expect(chunked).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]);
+  });
+});
