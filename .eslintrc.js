4e990e236816d75ee28662e3e9df053ea16c4c2a
Flavio Barboza <flavio.barboza@gmail.com>
2023-03-19 18:26:34 -0600

    typescript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,29 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'consistent-return': 1,
     'no-console': 0,
     'no-restricted-syntax': 0,
     'no-plusplus': 0,
+    'import/extensions': ['error', {
+      '.ts': 'never',
+    }],
+    'max-len': ['error', {
+      code: 100,
+      tabWidth: 2,
+    }],
+    '@typescript-eslint/no-explicit-any': 0,
   },
 };
