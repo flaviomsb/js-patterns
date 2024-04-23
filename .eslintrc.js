4902d769833e54026af38a43d9213f6ae22f3899
Flavio Barboza <flavio.barboza@gmail.com>
2024-04-22 22:39:54 -0600

    eslint-plugin-deprecation install

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'plugin:deprecation/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
