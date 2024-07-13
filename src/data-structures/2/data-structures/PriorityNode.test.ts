c6c31d88f2e5f07aca9939952d35e7043790aea2
Flavio Barboza <flavio.barboza@gmail.com>
2024-07-12 18:43:12 -0600

    priority node unit test

---
+++ src/data-structures/2/data-structures/PriorityNode.test.ts
@@ -0,0 +1,14 @@
+import { PriorityLevel, PriorityNode } from './PriorityNode';
+
+describe('PriorityNode', () => {
+  test('PriorityNode is class', () => {
+    expect(typeof PriorityNode.prototype.constructor).toEqual('function');
+  });
+
+  test('should hold passed data at instantiation', () => {
+    const priorityNode = new PriorityNode(5, PriorityLevel.High);
+
+    expect(priorityNode.value).toBe(5);
+    expect(priorityNode.priority).toBe(0);
+  });
+});
