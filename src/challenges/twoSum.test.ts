25c33ec4f5bd0c3a5b289f6d04b4a4d4cc2e7ade
Flavio Barboza <flavio.barboza@gmail.com>
2024-09-15 12:06:46 -0600

    twoSum challenge unit test

---
+++ src/challenges/twoSum.test.ts
@@ -0,0 +1,12 @@
+import { twoSum } from './twoSum';
+
+describe('twoSum', () => {
+  test.each([
+    [[2, 7, 11, 15], 9, [0, 1]],
+    [[3, 2, 4], 6, [1, 2]],
+    [[3, 3], 6, [0, 1]],
+    [[2, 3, 6], 4, []],
+  ])('should find the indexes in array %s that sum %s', (nums, target, output) => {
+    expect(twoSum(nums, target)).toEqual(output);
+  });
+});
