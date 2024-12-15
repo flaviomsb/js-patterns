089abcec9d0b278778d079266b0cf8ee7a6226a7
Flavio Barboza <flavio.barboza@gmail.com>
2024-12-14 22:47:48 -0600

    fix ioc test

--- src/challenges/ioc/NotificationManager.test.ts
+++ src/challenges/ioc/NotificationManager.test.ts
@@ -3,13 +3,13 @@ import { EmailService } from './EmailService';
 import { NotificationManager } from './NotificationManager';
 
 describe('NotificationManager IoC', () => {
-  test('should inject the service into the manager class', () => {
+  test('should inject the email service into the manager class', () => {
     const emailService = new EmailService();
     const emailManager = new NotificationManager(emailService);
 
     const spiedSendNotification = jest
       .spyOn(emailService, 'sendNotification')
-      .mockImplementation((message: string) => {});
+      .mockImplementation((_message: string) => {});
 
     emailManager.notify('email message sample');
 
