827e1a816aadf99c2020428e3a261dd2795dce41
Flavio Barboza <flavio.barboza@gmail.com>
2025-07-20 20:01:25 -0600

    numbersFrequency helper function

---
+++ src/coding-patterns/counting/numbersFrequency.ts
@@ -0,0 +1,13 @@
+type NumberFrequencyDict = {
+  [key: number]: number;
+};
+
+export function numbersFrequency(nums: number[]) {
+  return nums.reduce(
+    (freq: NumberFrequencyDict, num: number): NumberFrequencyDict => ({
+      ...freq,
+      [num]: freq[num] ? freq[num] + 1 : 1,
+    }),
+    {} as NumberFrequencyDict,
+  );
+}
