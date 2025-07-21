60c411269396e598a6c2b4561a55209f2733d8c4
Flavio Barboza <flavio.barboza@gmail.com>
2025-07-20 19:46:50 -0600

    added notes to maxFrequency coding challenge

--- src/coding-patterns/counting/maxFrequency.ts
+++ src/coding-patterns/counting/maxFrequency.ts
@@ -1,3 +1,21 @@
+/**
+ * Given an array of positive integers named nums, return the total frequencies of the elements in nums that have a maximum frequency.
+ *
+ * The frequency of an element is defined as the number of times that element is repeated in the array.
+ *
+ * Examples
+ * Example 1:
+ *
+ * Input: nums = [3, 2, 2, 3, 1, 4]
+ * Output: 4
+ * Explanation: Both 2 and 3 appear twice, which is the highest frequency. So, the total frequency is 2 + 2 = 4.
+ * Example 2:
+ *
+ * Input: nums = [5, 5, 5, 2, 2, 3]
+ * Output: 3
+ * Explanation: The number 5 appears three times, which is the highest frequency. So, the total frequency is 3.
+ **/
+
 type NumberFrequencyDict = {
   [key: number]: number;
 };
