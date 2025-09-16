f1d72af9f437bedb4f2e9cd4d8d7547540e2f3bb
Flavio Barboza <flavio.barboza@gmail.com>
2025-09-15 19:09:18 -0600

    tweaked eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,49 @@
+import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import jestEslint from 'eslint-plugin-jest';
 import globals from 'globals';
 
-export default tseslint.config({
-  ...eslint.configs.recommended,
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-  },
-  languageOptions: {
-    globals: {
-      ...globals.browser,
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    // enable jest rules on test files
+    files: ['**/*.test.{js,ts}'],
+    extends: [jestEslint.configs['flat/recommended']],
+    rules: {
+      'jest/no-focused-tests': 'warn',
     },
   },
-  rules: {
-    ...eslint.configs.recommended.rules,
-    'consistent-return': 1,
-    'no-console': 0,
-    'no-restricted-syntax': 0,
-    'no-plusplus': 0,
-    'no-const-assign': 'error',
-    'prefer-const': 'error',
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/ban-ts-comment': 0,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      'consistent-return': 1,
+      'no-console': 'off',
+      'no-restricted-syntax': 'off',
+      'no-plusplus': 'off',
+      'no-const-assign': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        }
+      ],
+    },
   },
-});
+);
