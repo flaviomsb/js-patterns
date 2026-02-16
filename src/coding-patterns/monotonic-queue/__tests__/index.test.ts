1d0a22ee38a2e614fa2c6b8dbabdba0de9fe817e
Flavio Barboza <flavio.barboza@gmail.com>
2026-02-15 18:46:02 -0600

    monotonic queue with increasing and decreasing functions

--- src/coding-patterns/monotonic-queue/__tests__/index.test.ts
+++ src/coding-patterns/monotonic-queue/__tests__/index.test.ts
@@ -1,28 +1,56 @@
 import { describe, test, expect } from '@jest/globals';
-import createMonotonicIncreasingQueue from '../createMonotonicIncreasingQueue';
+import {
+  createMonotonicIncreasingQueue,
+  createMonotonicDecreasingQueue,
+} from '..';
 
-describe('createMonotonicIncreasingQueue', () => {
-  test('createMonotonicIncreasingQueue is a function', () => {
+describe('monotonic-queue', () => {
+  test('module export functions', () => {
     expect(typeof createMonotonicIncreasingQueue).toEqual('function');
+    expect(typeof createMonotonicDecreasingQueue).toEqual('function');
   });
 
-  test.each([
-    [
-      [4, 8],
-      [7, 5, 6, 4, 8],
-    ],
-    [
-      [2, 7, 12],
-      [9, 3, 2, 8, 7, 12],
-    ],
-    [
-      [3, 8, 12],
-      [3, 21, 8, 15, 12],
-    ],
-  ])(
-    'should create queue %s from numbers list %s',
-    (expectedQueue: number[], nums: number[]) => {
-      expect(createMonotonicIncreasingQueue(nums)).toEqual(expectedQueue);
-    },
-  );
+  describe('createMonotonicIncreasingQueue', () => {
+    test.each([
+      [
+        [4, 8],
+        [7, 5, 6, 4, 8],
+      ],
+      [
+        [2, 7, 12],
+        [9, 3, 2, 8, 7, 12],
+      ],
+      [
+        [3, 8, 12],
+        [3, 21, 8, 15, 12],
+      ],
+    ])(
+      'should create an increasing queue %s from numbers list %s',
+      (expectedQueue: number[], nums: number[]) => {
+        expect(createMonotonicIncreasingQueue(nums)).toEqual(expectedQueue);
+      },
+    );
+  });
+
+  describe('createMonotonicDecreasingQueue', () => {
+    test.each([
+      [
+        [8],
+        [7, 5, 6, 4, 8],
+      ],
+      [
+        [12, 10],
+        [9, 3, 12, 8, 7, 10],
+      ],
+      [
+        [31, 19, 5],
+        [31, 8, 1, 19, 5],
+      ],
+    ])(
+      'should create a decreasing queue %s from numbers list %s',
+      (expectedQueue: number[], nums: number[]) => {
+        expect(createMonotonicDecreasingQueue(nums)).toEqual(expectedQueue);
+      },
+    );
+  });
 });
--- src/coding-patterns/monotonic-queue/index.ts
+++ src/coding-patterns/monotonic-queue/index.ts
@@ -1,11 +1,14 @@
-export default function createMonotonicIncreasingQueue(nums: number[]) {
+function createMonotonicFrom(
+  nums: number[],
+  fn: (num: number, deque: number[]) => boolean,
+): number[] {
   // Initialize an empty deque to store the elements
   const deque: number[] = [];
 
-  nums.forEach(num => {
+  nums.forEach((num) => {
     // Maintain order in the deque
-    // Remove elements from the back if they are greater than or equal to the current element
-    while (deque.length > 0 && deque[deque.length - 1] >= num) {
+    // Remove elements from the back
+    while (fn(num, deque)) {
       deque.pop(); // Remove the back element
     }
 
@@ -16,3 +19,19 @@ export default function createMonotonicIncreasingQueue(nums: number[]) {
   // Return the final deque representing the monotonic increasing queue
   return deque;
 }
+
+function createMonotonicIncreasingQueue(nums: number[]): number[] {
+  return createMonotonicFrom(
+    nums,
+    (num, deque) => deque.length > 0 && deque[deque.length - 1] >= num,
+  );
+}
+
+function createMonotonicDecreasingQueue(nums: number[]): number[] {
+  return createMonotonicFrom(
+    nums,
+    (num, deque) => deque.length > 0 && deque[deque.length - 1] <= num,
+  );
+}
+
+export { createMonotonicIncreasingQueue, createMonotonicDecreasingQueue };
